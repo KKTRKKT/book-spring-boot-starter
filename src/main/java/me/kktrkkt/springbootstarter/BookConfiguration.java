5a0f457ba950d573553e0cd7a4bea91511ff12f7
KKTRKKT <paulhana6006@gmail.com>
2023-05-22 17:21:17 +0900

    ConfigurationProperties 설정 추가

--- src/main/java/me/kktrkkt/springbootstarter/BookConfiguration.java
+++ src/main/java/me/kktrkkt/springbootstarter/BookConfiguration.java
@@ -1,18 +1,20 @@
 package me.kktrkkt.springbootstarter;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(BookProperties.class)
 public class BookConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public Book book(){
+    public Book book(BookProperties properties){
         Book book = new Book();
-        book.setTitle("스프링부트");
-        book.setPrice(15000);
+        book.setTitle(properties.getTitle());
+        book.setPrice(properties.getPrice());
         return book;
     }
 }
